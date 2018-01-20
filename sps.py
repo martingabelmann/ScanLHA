30b0eb5765bd1f8ea381a4bbe278fa5850745722
Martin Gabelmann <martin@gabelmann.biz>
2018-01-20 21:40:10 +0100

    speed up the scan class

--- sps.py
+++ sps.py
@@ -3,16 +3,18 @@
 
 import argparse
 import yaml
-import pyslha
+import pylha
 # import lzma
 import logging
 from subprocess import Popen, STDOUT, PIPE
-from collections import defaultdict
+from collections import defaultdict,ChainMap
 import os
-from numpy import arange
-import pandas
+from numpy import linspace, prod
 from concurrent.futures import ThreadPoolExecutor
-from itertools import combinations
+from itertools import product
+from random import randrange
+
+logging.getLogger().setLevel(logging.INFO)
 
 class Config(dict):
     def __init__(self,src):
@@ -31,99 +33,148 @@ def genSLHA(blocks):
     """generate SLHA"""
     out = ''
     for block in blocks:
-        out += 'Block {}\n'.format(block['block'])
+        out += 'BLOCK {}\n'.format(block['block'])
         for data in block['lines']:
             data = defaultdict(str,data)
             if 'scan' in data:
-                data['value'] = '{%' + str(data['id']) + '%}'
+                data['value'] = '{%' + str(data['id']) + block['block'] + '%}'
             out += '{id} {value} #{comment}\n'.format_map(data)
     return out
 
-def parseSLHA(slhafile):
-    """TODO: use something faster"""
+def parseSLHA(slhafile, blocks=[]):
     try:
-        rawslha = pyslha.read(slhafile)
-        blocks = { ''.join(block) : pandas.Series(dict(rawslha.blocks[block].items())) for block in rawslha.blocks }
-        # decays = pandas.Series({ ''.join(decay) : pandas.Series(dict(rawslha.decays[decay].items())) for decay in rawslha.decays })
-        # xsections = pandas.Series({ ''.join(x)  : pandas.Series(dict(rawslha.xsectionss[x].items())) for x in rawslha.xsections })
-        slha = blocks
-    except Exception as e:
-        print(str(e))
-        slha = None
-    return slha
-
+        with open(slhafile,'r') as f:
+            slha = pylha.load(f)
+    except FileNotFoundError:
+        logging.error('File ' + slhafile + ' not found.')
+        return -2
+    except:
+        print('Could not parse ' + slhafile + '!')
+        return -3
+    slha_block = slha['BLOCK']
+    if blocks:
+        slha_block = { b : v for b,v in slha_block.items() if b in blocks }
+    return slha_block
 
 class SPheno():
-    def __init__(self, conf, tpl):
+    def __init__(self, conf, tpl, blocks=[]):
         self.config = conf
+        self.timeout = conf.get('timeout', 10)
         self.tpl = tpl
+        self.blocks = blocks
         if 'slhadir' not in conf:
             self.config['slhadir'] = '/tmp/slha'
         if not os.path.exists(self.config['slhadir']):
             os.makedirs(self.config['slhadir'])
 
     def run(self, params):
-        fname = '_'.join(['{}.{}'.format(p,v) for p,v in params.items()])
-        fin  = "{}/SLHA.{}.in".format(self.config['slhadir'], fname)
-        fout = "{}/SLHA.{}.out".format(self.config['slhadir'], fname)
+        if type(params) != dict:
+            try:
+                params = dict(ChainMap(*params))
+            except:
+                logging.error('Could not read params: ', params)
+                return -5
+        # TODO: Too long filenames/argument for subprocess?
+        # fname = '_'.join(['{}.{}'.format(p,v) for p,v in params.items()])
+        fname = '%030x' % randrange(16**30)
+        fin  = "{}/{}.in".format(self.config['slhadir'], fname)
+        fout = "{}/{}.out".format(self.config['slhadir'], fname)
         with open(fin, 'w') as inputf:
             params = defaultdict(str, { '%{}%'.format(p) : v for p,v in params.items() })
             inputf.write(self.tpl.format_map(params))
 
         proc = Popen([self.config.get('binary', './SPheno'), fin, fout], stderr=STDOUT, stdout=PIPE)
-        pipe = proc.communicate(timeout=5)
+        pipe = proc.communicate(timeout=self.timeout)
+        log = ''
         for p in pipe:
             if not p:
                 continue
             with open(fout + '.log', 'w') as logf:
-                logf.write('parameters: {} \nmessage: {}'.format(
+                log += 'parameters: {} \nmessage: {}\n\n'.format(
                             str(params),
                             p.decode('utf8').strip()
-                            ))
+                        )
+                logf.write(log)
         if os.path.isfile(fout):
-            os.remove(fin)
-            return parseSLHA(fout)
+            slha = parseSLHA(fout, self.blocks)
+            if self.config.get('keep_slha', False):
+                os.remove(fout)
+                os.remove(fin)
+            return slha
+        elif os.path.isfile(fout + '.log'):
+            logging.warning(log)
+            return fname + '.log'
         else:
-            return fout + '.log'
+            return -4
 
 class Scan():
-    def __init__(self, c):
+    def __init__(self, c, getblocks=['MASS']):
         self.config = c
         self.template = genSLHA(c['blocks'])
-        self.spheno = SPheno(c['spheno'], self.template)
+        self.getblocks = getblocks
+        self.spheno = SPheno(c['spheno'], self.template, self.getblocks)
         self.scanset = []
-        self.parameters = {}
+        self.scanparas = defaultdict(defaultdict)
         for block in c['blocks']:
             for p in block['lines']:
                 if 'scan' in p:
-                    self.registerscan(p['id'],p['scan'])
-        if not self.parameters:
-            logging.warning("No scan parameters given!")
+                    self.scanparas[block['block']][p['id']] = p['scan']
+        if not self.scanparas:
+            logging.warning("No scan parameters defined in condig!")
+            logging.warning("Register a scan with registerScan(<block>, <para-id>, [<start>,<end>,<stepsize>])")
+
+    def addToTemplate(self, block, paraid, scanrange, parameter=None, comment=None):
+        # add scan-parameter to config (if not already)
+        if len(scanrange) != 3:
+            logging.error("No proper 'scan' option set for parameter: " + paraid)
+            return
+        if 'blocks' not in self.config:
+            logging.error("No 'blocks' section in config ")
+            return
+        blockpos = [i for i,b in enumerate(self.config['blocks']) if b == block]
+        if len(blockpos) != 1:
+            logging.error('Block "' + block + ' should appear exactly once in Block ' + block)
+        lines = self.config['blocks'][blockpos[0]]['lines']
+        linepos = [i for i,l in enumerate(lines) if 'id' in l and l['id'] == paraid]
+        if not linepos:
+            lines.append({'parameter':parameter,'id':paraid,'scan':scanrange,'comment':comment})
+        elif len(linepos) > 1:
+            logging.error('Parameter with id ' + paraid + ' has multiple occurence in config')
+            return
+        else:
+            if not parameter and 'parameter' in lines[linepos[0]]:
+                parameter = lines[linepos[0]]['parameter']
+            if not comment and 'comment' in lines[linepos[0]]:
+                comment = lines[linepos[0]]['comment']
+            lines[linepos[0]] = {'parameter':parameter,'id':paraid,'scan':scanrange,'comment':comment}
+
+        # update the slha template with new config
+        self.template = genSLHA(self.config['blocks'])
+        return self.template
+
+    def buildScanset(self, maxram=0.1, threads=4):
+        numparas = prod([p[2] for paras in self.scanparas.values() for p in paras.values()])
+        logging.info('Expanding scan ranges for scan parameters.')
+        values = []
+        for block,paras in self.scanparas.items():
+            for p,scan in paras.items():
+                values.append([{str(p) + block: num} for num in linspace(*scan)])
+
+        logging.info('Build all ' + str(numparas) + ' combinations.')
+        self.scanset = list(product(*values))
+        return numparas
+
+    def registerScan(self, block, paraid, scanrange, parameter=None, comment=None):
+        if self.addToTemplate(block,paraid,scanrange,parameter,comment):
+            self.scanparas[block][paraid] = scanrange
+            return self.scanparas
+        return
 
     # TODO:
     # ability to split/parallelize scan-range over qsub
-    def registerscan(self,ID,scanrange):
-        """
-        self.scanset = [{'param1':'value1','param2':'value2'},{'param1':'value3','param2':'value4'},...]
-        """
-        if len(scanrange) == 3:
-            self.parameters.update({ID:scanrange})
-        else:
-            logging.error("No proper 'scan' option set for parameter: " + ID)
-        values = [ [id,i] for id,scan in self.parameters.items() for i in arange(*scan)]
-        allcombinations = combinations(values, len(self.parameters))
-        self.scanset = []
-        for c in allcombinations:
-            s = { id:value for id,value in c}
-            if len(s) < len(self.parameters):
-                continue
-            self.scanset.append(s)
-
     def submit(self,w):
         with ThreadPoolExecutor(w) as executor:
-            self.results = executor.map(self.spheno.run, self.scanset)
-            self.data = pandas.Series([ { 'parameter': p, 'blocks': v} for p,v in zip(self.scanset, self.results)])
-        return self.data
+            self.results = zip(self.scanset,executor.map(self.spheno.run, self.scanset))
 
     def save(self):
         pass
@@ -131,7 +182,7 @@ class Scan():
 class Plot():
     def __init__(self,data):
         pass
-    
+
     def show():
         pass
 
