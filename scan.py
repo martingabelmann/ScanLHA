153676d669c262f42454fb54be55249b68fb375a
Martin Gabelmann <martin@gabelmann.biz>
2018-04-20 10:32:24 +0200

    adapt changes in example script

--- scan.py
+++ scan.py
@@ -1,111 +1,79 @@
 #!/usr/bin/env python3
-import ScanLHA
-import os
-import matplotlib
+from IPython import embed
 from pandas import read_hdf
-from collections import OrderedDict
-from scipy.interpolate import interp1d
+import matplotlib
 matplotlib.use('Agg')
-import matplotlib.pyplot as plt # pylint: disable=E402
+from matplotlib import pyplot as plt
+from ScanLHA import ScanLHA
+from ScanLHA.ScanLHA import getKey
 
 # Definitions
-MINPARID = OrderedDict()
-MINPARID['NMSSM'] = { 'At':1, 'LambdaS':2, 'MSUSY':3, 'TanBeta':4}
-MINPARID['NMSSMLO'] = { 'At':1, 'LambdaS':2, 'MSUSY':3, 'TanBeta':4}
-MINPARID['MSSM']  = { 'At':1, 'MSUSY':2, 'TanBeta':3 }
+BLOCKS = ScanLHA.BLOCKS
+BIN = ScanLHA.BIN
+VALUES = ScanLHA.VALUES
+LATEX = ScanLHA.LATEX
+ScanLHA.CONF = ScanLHA.Config('ScanLHA/configs/SPheno.yml')
+cmap = plt.get_cmap("tab10")
 
-MARKER = { 'NMSSM': 'o', 'NMSSMLO': '+'}
+BLOCKS['NMSSM']['MINPAR']   = { 'At':1, 'LambdaS':2, 'MSUSY':3, 'TanBeta':4}
+BLOCKS['MSSM']['MINPAR']    = { 'At':1, 'MSUSY':2, 'TanBeta':3 }
 
-MINPARVAL = {
-        # 'At': {'values': ["{3MINPAR}/{4MINPAR}", 0, "3.5*{3MINPAR}/{4MINPAR}"] },
-        'At': { 'value': 0 },
-        'TanBeta': { 'values': [1,4] },
-        'MSUSY': { 'scan': [1e3, 1e16, 50], 'distribution': 'geom' },
-        'LambdaS': { 'values': [0.1,0.3,0.4] }
-        }
+BIN['NMSSM'] = '../bin/SPhenoNMSSM'
+BIN['MSSM'] = '../bin/SPhenoMSSM'
 
-LATEX = {
-        'At' : "A_t",
-        'TanBeta':  "\\tan(\\beta)",
-        'MSUSY': "M_{SUSY}",
-        "LambdaS": "\lambda_s"
-        }
+VALUES['At'] = { 'value': 0 }
+VALUES['TanBeta'] = { 'values': [1,1.1,1.2,1.3,1.4,1.5,1.6,1.7,1.8,1.9,2,3,4] }
+VALUES['MSUSY'] = { 'scan': [1e3, 1e16, 50], 'distribution': 'geom' }
+VALUES['LambdaS'] = { 'values': [0,0.2,0.3,0.6] }
+
+LATEX['At'] = "A_t"
+LATEX['TanBeta'] = "\\tan\\beta"
+LATEX['MSUSY'] = "M_{SUSY}"
+LATEX['LambdaS'] = "\lambda_s"
 
 HDFSTORE = 'scans/MSSMvsNMSSM/store.h5'
 
-# Load default config
-c = ScanLHA.Config('config.yml')
-
-# Helper functions
-def runscan(c, model):
-    c.setBlock('MINPAR') # clear previous entries
-    for para,value in MINPARVAL.items():
-        if para not in MINPARID[model]:
-            print('Parameter {} skipped for {}'.format(para, model))
-            continue
-        paraid = MINPARID[model][para]
-        line = { 'parameter': para, 'id': paraid }
-        line.update(value)
-        c.setLine('MINPAR', line)
-
-    c['spheno']['binary'] = '../bin/SPheno' + model
-    print('scanning ' + model + ' ...')
-    scan = ScanLHA.Scan(c,getblocks=[])
-    scan.submit()
-    if 'log' in scan.results:
-        failed = len([ r for r in scan.results['log'] if type(r) == str ])
-        print("{} out of {} points are invalid.".format(failed, scan.numparas))
-    return scan.results
-
-def getSubPlot(model, filters):
-    r = results[model]
-    for para,value in filters.items():
-        paraid = 'MINPAR.values.%d' % MINPARID[model][para]
-        r = r.loc[r[paraid] == value]
-    key = 'MINPAR.values.%d' % MINPARID[model]['MSUSY']
-    return (list(r[key]), list(r['MASS.values.25']))
-
 # Scans
-skip = 'r'
-if os.path.exists(HDFSTORE):
-    skip = input('Old scans found. Delete and rescan [r] or skip scans [s]? (enter = rescan)')
-
-if skip == 'r' or skip == '':
-    results = ScanLHA.HDFStore(HDFSTORE)
-    [ results.put(model, runscan(c,model)) for model in MINPARID.keys() ]
-    results.close()
-
-if skip == 's':
-    # read into RAM to speed up the plots
-    results = { model : read_hdf(HDFSTORE, model) for model in MINPARID.keys() }
-
+ScanLHA.runAll(HDFSTORE)
+results = {}
+results["NMSSM"] = read_hdf(HDFSTORE,"NMSSM")
+results["MSSM"] = read_hdf(HDFSTORE,"MSSM")
 # Plots: one plot for each value of TanBeta
 print('Generating plots.')
 plt.rcParams.update({'font.size': 15})
-for tanb in MINPARVAL['TanBeta']['values']:
+for tanb in VALUES['TanBeta']['values']:
+    c = 0
     fig = plt.figure()
     plt.rc('text', usetex=True)
-    plt.xlabel(r'$M_{SUSY}$ / GeV')
-    plt.ylabel(r'$m_{H}$ / GeV')
     ax = plt.gca()
     ax.plot([],[],' ', marker=None, label="$\\tan\\beta=" + str(tanb) + "$")
     ax.legend()
-    plt.axhline(y=125, color='r', linestyle='-', label="125 GeV")
+    plt.axhline(y=125, color='r', linestyle='-', label="125 GeV", zorder=99)
     Label = ''
-    for model,minpar in MINPARID.items():
-        if 'LambdaS' not in minpar.keys():
-            mhmsusy = getSubPlot(model,{'TanBeta': tanb})
-            Label = model
-            mhms = interp1d(mhmsusy[0],mhmsusy[1])
-            ax.plot(mhmsusy[0],mhms(mhmsusy[0]), label=Label, c='black')
-            continue
-        for lams in MINPARVAL['LambdaS']['values']:
-            Label = model + ': '
-            mhmsusy = getSubPlot(model,{'TanBeta': tanb, 'LambdaS': lams})
-            Label = model + ': $\lambda_s = ' + str(lams) + '$'
-            ax.scatter(mhmsusy[0],mhmsusy[1], label=Label, s=25, marker=MARKER[model])
+    for model,blocks in BLOCKS.items():
+        x = getKey(model,'MSUSY')
+        y = 'MASS.values.25'
+        for block,minpar in blocks.items():
+            if 'LambdaS' not in minpar.keys():
+                r = results[model]
+                r = r[ r[getKey(model,'TanBeta')] == tanb ]
+                mhmsusy = r[[x, y]]
+                mhmsusy = mhmsusy.sort_values(by=x)
+                mhmsusy.plot(y=y, x=x, kind='scatter', ax=ax, color="C{}".format(c),zorder=c, label=model)
+                c += 1
+                continue
+            for lams in VALUES['LambdaS']['values']:
+                r = results[model]
+                r = r[(r[getKey(model,'TanBeta')] == tanb) & (r[getKey(model,'LambdaS')] == lams)]
+                mhmsusy = r[[x, y]]
+                mhmsusy = mhmsusy.sort_values(by=x)
+                Label = model + ': '
+                Label = model + ': $\lambda_s = ' + str(lams) + '$'
+                mhmsusy.plot(y=y, x=x, ax=ax, color="C{}".format(c), zorder=c, label=Label)
+                c += 1
     plt.legend(loc='lower right')
     ax.set_xscale('log')
-    print('TanBeta = %d'%tanb)
+    ax.set(xlabel=r'$M_{SUSY}$ / GeV', ylabel=r'$m_{H}$ / GeV')
+    print('TanBeta = {}'.format(tanb))
     fig.savefig('scans/MSSMvsNMSSM/TanBeta' + str(tanb) + '.svg' ,bbox_inches='tight')
 print('Done.')
