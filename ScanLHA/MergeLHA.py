4db299ab5c0ed82ffd1a23b2318f6d33b0eaf179
Martin Gabelmann <martin@gabelmann.biz>
2018-10-27 18:28:33 +0200

    MergeLHA docstring

--- ScanLHA/MergeLHA.py
+++ ScanLHA/MergeLHA.py
@@ -1,17 +1,37 @@
 #!/usr/bin/env python3
+"""
+Merges multiple HDF files into one file.
+"""
 from pandas import HDFStore, DataFrame
 from glob import glob
 import sys
+from os import getenv
+__pdoc__ = {}
+__pdoc__['Merge'] = """
+Merges multiple HDF files into one file.
 
-def main():
+Usage: MergeLHA mergedfile.h5 file1.h5 file2.h5 [...]
+
+File names may be specified using patterns compatible with python.glob (e.g. '*.h5').
+
+Note that it is not possible to merge different `ScanLHA.config.Config` instances i.e. the `Config`
+instance of the first file is used.
+
+
+The default H5 tree is 'results' (default of `ScanLHA.scan.Scan.save`).
+For changing the path set the environment variable `export LHPATH='/yourpath'`.
+"""
+
+def Merge():
     if len(sys.argv) < 3:
-        print('No valid filenames given!\nUsage: MergeLHA mergedfile.h5 file1.h5 file2.h5 [...]')
+        print('No valid filenames given!')
+        print(__pdoc__['Merge'])
         sys.exit(1)
-
-    path = 'results'
 
     infiles = [glob(f) for f in sys.argv[1:-1]]
     outfile = sys.argv[-1]
+
+    LHAPATH = getenv('LHAPATH') if getenv('LHAPATH') else 'results'
 
     print("Will concatenate into {}.".format(outfile))
     store = HDFStore(outfile)
@@ -22,9 +42,9 @@ def main():
         for f in fs:
             print('Reading %s ...' % f)
             tmp_store = HDFStore(f)
-            tmp_conf = tmp_store.get_storer(path).attrs.config
+            tmp_conf = tmp_store.get_storer(LHAPATH).attrs.config
             try:
-                tmp_conf = tmp_store.get_storer(path).attrs.config
+                tmp_conf = tmp_store.get_storer(LHAPATH).attrs.config
                 if not store_conf:
                     store_conf = tmp_conf
             except AttributeError:
@@ -35,13 +55,13 @@ def main():
                 print('Warning: merge file with different config {}'.format(f))
             tmp_df = tmp_store['results']
             try:
-                tmp_df['scan_seed'] = tmp_store.get_storer(path).attrs.seed
-                tmp_df['scan_parallel'] = tmp_store.get_storer(path).attrs.parallel
+                tmp_df['scan_seed'] = tmp_store.get_storer(LHAPATH).attrs.seed
+                tmp_df['scan_parallel'] = tmp_store.get_storer(LHAPATH).attrs.parallel
             except AttributeError:
                 pass
             df = df.append(tmp_df, ignore_index=True)
             tmp_store.close()
 
-    store[path] = df
-    store.get_storer(path).attrs.config = store_conf
+    store[LHAPATH] = df
+    store.get_storer(LHAPATH).attrs.config = store_conf
     store.close()
