84cc0252ca9ac362194ff218a4802099abd2b075
Martin Gabelmann <martin@gabelmann.biz>
2018-10-27 16:41:10 +0200

    Scan module docstrings

--- ScanLHA/scan.py
+++ ScanLHA/scan.py
@@ -29,10 +29,16 @@ def substitute(param_dict):
     else:
         return substitute(subst)
 
+__all__ = ['Scan', 'RandomScan']
+
 class Scan():
     """ Scan object
+
     Controls a scan over a n-dimensional parameter range using a grid.
-    Needs a Config object (see ?ScanLHA.Config) for initialization
+
+    Needs a Config object (see `ScanLHA.config.Config`) for initialization.
+
+    The `'runner'` config must be present as well.
     """
     def __init__(self, c):
         self.config = c
@@ -55,13 +61,15 @@ class Scan():
             logging.info("Register a list of scan values with  addScanValues(<block>,{'id': <para-id>, 'values': [1,3,6,...])")
 
     def addScanRange(self, block, line):
-        """ register a scan range for SLHA entry in the block <block>.
-        <line> must be a dict containing
-          * the SLHA id 'id' within the block <block>
-          * a scan range 'scan' consiting of a two-tuple
-          * optional: 'distribution'
-            can be 'log', 'linear', 'geom', 'arange', 'uniform' and 'normal', see ?ScanLHA/Config.distribution
-            default: 'linear'
+        """ Register a scan range for LHA entry in the block <block>.
+
+        `'line'` must be a dict containing:
+
+          1. the LHA id 'id' within the block `block`
+          2. a scan range 'scan' consisting of a two-tuple
+          3. optional: `'distribution'`
+            can be `'log'`, `'linear'`, `'geom'`, `'arange'`, `'uniform'` and `'normal'`.
+            default: `'linear'`
         """
         if 'id' not in line:
             logging.error("No 'id' set for parameter.")
@@ -75,10 +83,13 @@ class Scan():
         self.addScanValues(block, line)
 
     def addScanValues(self, block, line):
-        """ set a SLHA entry to a specific list of input values in the block <block>.
-            <line> must be a dict containing the
-            * 'id' of the SLHA entry within the block
-            * 'values' given as a list
+        """
+        Set a LHA entry to a specific list of input values in the block `block`.
+
+        `line` must be a dict containing the:
+
+          1. `id` of the LHA entry within the block
+          2. `values` given as a list
         """
         if 'id' not in line:
             logging.error("No 'id' set for parameter.")
@@ -91,7 +102,7 @@ class Scan():
         self.config['runner']['template'] = genSLHA(self.config['blocks'])
 
     def build(self,num_workers=4):
-        """ expand parameter lists and scan ranges while substituting eventual dependencies. """
+        """ Expand parameter lists and scan ranges while substituting eventual dependencies. """
         if not self.config.validate():
             return
         values = []
@@ -109,16 +120,19 @@ class Scan():
         return
 
     def scan(self, dataset):
-        """ register an runner using the config and apply it on <dataset> """
+        """ Register an runner using the config and apply it on `dataset` """
         # this is still buggy: https://github.com/tqdm/tqdm/issues/510
         # res = [ runner.run(d) for d in tqdm(dataset) ]
         runner = self.runner(self.config['runner'])
         return concat([ runner.run(d) for d in dataset ], ignore_index=True)
 
     def submit(self,num_workers=None):
-        """ Start a scan and distribute it on <num_workers> threads.
-        If <num_workers> is omitted, the value of os.cpu_count() is used.
-        Results are stored in self.results.
+        """
+        Start a scan and distribute it on `num_workers` threads.
+
+        If `num_workers` is omitted, the value of `os.cpu_count()` is used.
+
+        Results are stored in `self.results`.
         """
         num_workers  = os.cpu_count() if not num_workers else num_workers
         if not self.scanset:
@@ -141,7 +155,7 @@ class Scan():
         self.results = concat(self.results, ignore_index=True)
 
     def save(self, filename='store.hdf', path='results'):
-        """ Saves self.results into the HDF file <filename> in the tree <path>. """
+        """ Saves `self.results` into the HDF file `filename` in the tree `path`. """
         print('Saving to {} ({})'.format(filename,path))
         if path == 'config':
             logging.error('Cant use "config" as path, using "config2" instead.i')
@@ -153,9 +167,12 @@ class Scan():
 
 class RandomScan():
     """ Scan object
-    Controls a scan over a n-dimensional parameter range using uniformly distributed numbers.
-    Needs a Config object (see ?ScanLHA.Config) for initialization.
-    The 'runner' config-entry needs to specify the number 'numparas' of randomly generated parameters.
+
+    Controls a scan over an n-dimensional parameter range using uniformly distributed numbers.
+
+    Needs a Config object (see `ScanLHA.config.Config`) for initialization.
+
+    The `'runner'` config-entry needs to specify the number `'numparas'` of randomly generated parameters.
     """
 
     def __init__(self, c, seed=None):
@@ -164,21 +181,27 @@ class RandomScan():
         self.config['runner']['template'] = genSLHA(c['blocks'])
         self.getblocks = self.config.get('getblocks', [])
         self.runner = RUNNERS[self.config['runner'].get('type','RunnerSLHA')]
-        self.parallel = 1
+        self.parallel = os.cpu_count()
         self.seed = round(time()) if not seed else seed
         self.randoms = { p : [eval(str(k)) for k in v['random']] for p,v in c.parameters.items() if 'random' in v }
         self.dependent = { p : v['value'] for p,v in c.parameters.items() if v.get('dependent',False) and 'value' in v }
 
     def generate(self):
-        """ generate uniformly distributed numbers for specified SLHA blocks.
-        Substitute the numbers in dependend blocks if necessary.
+        """
+        Generate uniformly distributed numbers for specified SLHA blocks.
+
+        Substitute the numbers in dependend blocks, if necessary.
         """
         dataset = { p : v for p,v in self.dependent.items() }
         [ dataset.update({ p : uniform(*v)}) for p,v in self.randoms.items() ]
         return substitute(dataset)
 
     def scan(self, numparas, pos=0):
-        """ register an runner using the config and generate <numparas> data samples """
+        """
+        Register a runner using the config and generate `numparas` data samples.
+
+        Returns a `pandas.DataFrame`.
+        """
         numresults = 0
         runner = self.runner(self.config['runner'])
         if not runner.initialized:
@@ -196,9 +219,12 @@ class RandomScan():
         return concat(results, ignore_index=True)
 
     def submit(self,num_workers=None):
-        """ Start a scan and distribute it on <num_workers> threads.
-        If <num_workers> is omitted, the value of os.cpu_count() is used.
-        Results are stored in self.results.
+        """
+        Start a scan and distribute it on `num_workers` threads.
+
+        If `num_workers` is omitted, the value of `os.cpu_count()` is used.
+
+        Results are stored in `self.results`.
         """
         num_workers = os.cpu_count() if not num_workers else num_workers
         self.parallel = num_workers
@@ -217,7 +243,7 @@ class RandomScan():
         self.results = concat(self.results, ignore_index=True)
 
     def save(self, filename='store.hdf', path='results'):
-        """ Saves self.results into the HDF file <filename> in the tree <path>. """
+        """ Saves `self.results` into the HDF file `filename` in the tree `path`. """
         if self.results.empty:
             return
         print('Saving to {} ({})'.format(filename,path))
@@ -232,8 +258,10 @@ class RandomScan():
         store.close()
 
 class FileScan(Scan):
-    """ Performs a scan based on input file(s) from a previous scan
-    Needs a Config object (see ?ScanLHA.Config) for initialization
+    """
+    Performs a scan based on input file(s) from a previous scan.
+
+    Needs a Config object (see `ScanLHA.config.Config`) for initialization.
     """
     def __init__(self, c):
         self.config = c
