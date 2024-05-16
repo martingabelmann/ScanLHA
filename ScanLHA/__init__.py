848bbbbf2f22ff34722e9e7d0fa29bf090cc03c9
Martin Gabelmann <martin@gabelmann.biz>
2024-05-16 22:49:21 +0200

    bump to 0.5

--- ScanLHA/__init__.py
+++ ScanLHA/__init__.py
@@ -167,5 +167,5 @@ from .scan import Scan, RandomScan, FileScan
 from .config import Config
 from  .runner import RUNNERS
 from  .slha import genSLHA, parseSLHA
-__version__ = '0.4'
+__version__ = '0.5'
 __all__ = []
